f246343268c2ebe1ba7c9691db3890a6482f2a20
Eon <eoneon123@gmail.com>
2020-05-13 16:26:43 -0700

    added builder method for top-level lib class Numbering

--- app/models/lib/numbering.rb
+++ app/models/lib/numbering.rb
@@ -1,31 +1,43 @@
 class Numbering
   include Context
 
+  def self.builder
+    SelectMenu.builder(field_name: decamelize(self.slice_class(0)), options: self.subclasses.map {|klass| klass.builder})
+  end
+
   class Numbered < Numbering
-    def self.build_set
+    def self.builder
       klass_name = decamelize(self.slice_class(-1))
-      Edition.builder(klass_name)
-      # select_field = SelectField.builder(f={field_name: 'numbering-options', options: Option.builder(Proof.build_option_names(klass_name))})
-      # f={field_name: klass_name, options: [select_field, NumberField.builder(f={field_name:'edition_number'}), NumberField.builder(f={field_name:'edition_size'})]}
-      # FieldSet.builder(f)
+      f={field_name: klass_name, options: [Edition.select_field(klass_name), NumberField.builder(f={field_name:'edition_number'}), NumberField.builder(f={field_name:'edition_size'})]}
+      FieldSet.builder(f)
     end
   end
 
   class RomanNumbered < Numbering
-    def self.build_set
+    def self.builder
+      klass_name = decamelize(self.slice_class(-1))
+      f={field_name: klass_name, options: [Edition.select_field(klass_name), TextField.builder(f={field_name:'edition_number'}), TextField.builder(f={field_name:'edition_size'})]}
+      FieldSet.builder(f)
     end
   end
 
   class JapaneseNumbered < Numbering
-    def self.build_set
+    def self.builder
+      Edition.select_field(decamelize(self.slice_class(-1)))
     end
   end
 
+  class ProofEdition < Numbering
+    def self.builder
+      Edition.select_field(decamelize(self.slice_class(-1)))
+    end
+  end
+
+  ##############################################################################
+
   module Edition
-    def self.builder(klass_name)
-      select_field = SelectField.builder(f={field_name: select_field_name(klass_name), options: Option.builder(Proof.build_option_names(klass_name))})
-      f={field_name: klass_name, options: [select_field, NumberField.builder(f={field_name:'edition_number'}), NumberField.builder(f={field_name:'edition_size'})]}
-      FieldSet.builder(f)
+    def self.select_field(klass_name)
+      SelectField.builder(f={field_name: select_field_name(klass_name), options: Option.builder(Proof.build_option_names(klass_name))})
     end
 
     def self.select_field_name(klass_name)
@@ -40,7 +52,7 @@ class Numbering
 
     def self.build_option_names(klass_name)
       if klass_name == 'proof edition'
-        set.compact.map {|proof| "from #{format_vowel('a', proof)} #{proof} edition"}
+        set.reject {|i| i.blank?}.map {|proof| "from #{format_vowel('a', proof)} #{proof} edition"}
       else
         set.map{|opt| [opt, klass_name].reject {|i| i.nil?}.join(" ").strip}
       end
